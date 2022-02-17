f9da1d8ae33747e443a481720cadc663e04c8420
jayakrishnanc2020@gmail.com <jayakrishnanc2020@gmail.com>
2022-02-17 11:46:56 +0530

    delete unused toast

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -10,9 +10,7 @@ sap.ui.define([
 		return Controller.extend("project1234.controller.Home", {
 			onInit: function () {
 sap.m.MessageToast.show("Inside onInit Function Sucess");
-sap.m.MessageToast.show("Inside onInit Function third");
-sap.m.MessageToast.show("Inside onInit Function success");
-sap.m.MessageToast.show("Inside onInit Function feature");
+
 			}
 		});
 	});
